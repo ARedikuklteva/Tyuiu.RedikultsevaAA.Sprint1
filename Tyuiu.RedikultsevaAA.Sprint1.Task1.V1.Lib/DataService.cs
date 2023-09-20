04d3d66d55a8fddbda302f8293d70a442456ae96
ARedikultseva <anredikultseva@gmail.com>
2023-09-20 22:40:59 +0500

    Task1

---
+++ Tyuiu.RedikultsevaAA.Sprint1.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.RedikultsevaAA.Sprint1.Task1.V1.Lib
+{
+    public class DataService
+    {
+        public double Calculate(double x, double y, double a)
+        {
+            return x / 3 / y + 6 * a;
+        }
+    }
+}
---
+++ Tyuiu.RedikultsevaAA.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RedikultsevaAA.Sprint1.Task1.V1.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 6.0;
+            double y = 2.0;
+            double a = 1.0;
+            var res = ds.Calculate(x, y, a);
+            Assert.AreEqual(7, res);
+        }
+    }
+}
