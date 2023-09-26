817c27b11e3751a23a92a136a4387b3e83f7bb88
ARedikultseva <anredikultseva@gmail.com>
2023-09-26 17:15:13 +0500

    Task4

---
+++ Tyuiu.RedikultsevaAA.Sprint1.Task4.V6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RedikultsevaAA.Sprint1.Task4.V6.Lib;
+
+namespace Tyuiu.RedikultsevaAA.Sprint1.Task4.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Редикульцева А.А. | АСОиУб 23-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                  *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                           *");
+            Console.WriteLine("* Задание #4                                                                 *");
+            Console.WriteLine("* Вариант #6                                                                 *");
+            Console.WriteLine("* Выполнила: Редикульцева А.А. | АСОиУб 23-1                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,    *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.                *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                                 *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("abs(y ^ 2 - x) / x * y = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
